9b63ffa6d52e2d5c62e3a7e32820fe14cee860bc
ThunderSL94 <SL-Thunder@gmx.de>
2017-01-22 18:42:24 +0100

    BasiControl  - some changes

--- src/main/java/flee_and_catch/robot/communication/command/CommandType.java
+++ src/main/java/flee_and_catch/robot/communication/command/CommandType.java
@@ -1,5 +1,5 @@
 package flee_and_catch.robot.communication.command;
 
 public enum CommandType {
-	Connection, Synchronization, Control, Synchron, Follow, Exception
+	Connection, Synchronization, Control, Exception
 }
