76de6fcc4fb6721ae780700058d21033dfe10d5f
ThunderSL94 <SL-Thunder@gmx.de>
2017-02-13 23:49:09 +0100

    Control fixes  - fixes of refactoring and view

--- src/main/java/flee_and_catch/robot/configuration/CommunicationConfig.java
+++ src/main/java/flee_and_catch/robot/configuration/CommunicationConfig.java
@@ -2,6 +2,6 @@ package flee_and_catch.robot.configuration;
 
 public class CommunicationConfig {
 
-	public static final String BACKEND_ADDRESS = "192.168.0.100";
+	public static final String BACKEND_ADDRESS = "192.168.0.101";
 	public static final int BACKEND_PORT = 5000;
 }
